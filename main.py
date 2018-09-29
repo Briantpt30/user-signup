6cf52bf6846d0e360eb6554bd9ebf11846b0fbf6
Brian Quezada <briantpt30@gmail.com>
2018-09-29 12:42:32 -0400

    added functions stuck

--- main.py
+++ main.py
@@ -26,6 +26,51 @@ def no_spaces(text):
     else:
         return True
 
+def verify_pw(pw, verify):
+    if pw == verify:
+        return True
+    else:
+        return False    
+
+def verify_email(email):
+    if '@' in email and '.' in email:
+        return True
+    else:
+        return False    
+
+@app.route('/', methods=['POST'])
+def validate_info():
+
+    username = request.form['username']
+    password = request.form['password']
+    verify_pass = request.form['verify']
+    email = request.form['email']
+
+    username_error = ''
+    pw_error = ''
+    verify_error = ''
+    email_error = ''
+
+    if valid_text_length(username) == False and no_spaces(username) == False:
+        username_error = "That's not a valid username"
+    if valid_text_length(password) == False and no_spaces(password) == False:
+        pw_error= "That's not a valid password"
+        password=''
+    if verify_pw(password, verify_pass) == False:
+        verify_error="Passwords dont't match"
+        verify_pass=''
+    if verify_email(email) == False:
+        email_error = "That's not a valid email"
+        
+
+
+
+
+
+
+
+
+
 
 
 app.run()    
